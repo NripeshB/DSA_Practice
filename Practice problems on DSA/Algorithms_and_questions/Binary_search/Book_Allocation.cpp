db27646faaff92bfe7dffb34e08782702d1e84c5
Nripesh <nripesh.bhusal@gmail.com>
2024-08-14 17:50:34 +0530

    This program allocates minimum number of net books to n students.

--- Practice problems on DSA/Algorithms_and_questions/Binary_search/Book_Allocation.cpp
+++ Practice problems on DSA/Algorithms_and_questions/Binary_search/Book_Allocation.cpp
@@ -1,54 +1,74 @@
-#include<vector>
+//https://www.naukri.com/code360/problems/allocate-books_1090540?count=25&page=1&search=&sort_entity=order&sort_order=ASC&leftPanelTabValue=SUBMISSION
+//The code below is the solution for the question above
+
+#include <iostream>
+#include <vector>
 using namespace std;
 
-bool isPossible(vector<int> arr, int n, int m, int mid) {
-    int studentCount = 1;
-    int pageSum = 0;
-    //cout << "checking for mid "<< mid <<endl;
-    
-    for(int i = 0; i<n; i++) {
-        if(pageSum + arr[i] <= mid) {
-            pageSum += arr[i];
+
+bool ispossible(vector<int> &arr, int m, int n, int Mid) {
+    int pagecount = 0;
+    int studentcount = 1;
+    for(int i = 0; i < n; i++){
+        if(pagecount + arr[i] <= Mid){
+            pagecount += arr[i];
+        } else {
+            studentcount++;
+            if(studentcount > m || arr[i] > Mid){
+                return false;
+            }
+            pagecount = arr[i];
         }
-        else
-        {
-            studentCount++;
-            if(studentCount > m || arr[i] > mid ) {
-            return false;
-        }
-            pageSum = arr[i];
-        }
-        if(studentCount > m) {
-            return false;
-        }
-        //cout << " for i " << i << " Student "<< studentCount << " pageSum " << pageSum << endl;
     }
     return true;
 }
 
-int allocateBooks(vector<int> arr, int n, int m) {
-    int s = 0;
+int findPages(vector<int>& arr, int n, int m) {
     int sum = 0;
-    
-    for(int i = 0; i<n; i++) {
+    if (m > n) {
+        return -1;
+    }
+    for (int i = 0; i < n; i++) {
         sum += arr[i];
     }
+
+    int s = 0;
     int e = sum;
     int ans = -1;
-    int mid = s + (e-s)/2;
-    
-    while(s<=e)
-    {
-        if(isPossible(arr,n,m,mid)) {
-            //cout<<" Mid returned TRUE" << endl;
-            ans = mid;
-            e = mid - 1;
+    while(s <= e){
+        int Mid = s + ((e - s) / 2); // Corrected Mid calculation
+        if(ispossible(arr, m, n, Mid)){
+            ans = Mid;
+            e = Mid - 1;
+        } else { 
+            s = Mid + 1;
         }
-        else
-        {
-            s = mid + 1;
-        }
-        mid = s + (e-s)/2;
     }
     return ans;
+}
+
+
+int main() {
+    int n, m;
+    cout << "Enter the number of books: ";
+    cin >> n;
+    
+    cout << "Enter the number of students: ";
+    cin >> m;
+
+    vector<int> arr(n);
+    cout << "Enter the number of pages in each book: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int result = findPages(arr, n, m);
+
+    if (result == -1) {
+        cout << "It is not possible to allocate books to students." << endl;
+    } else {
+        cout << "The minimum number of pages that can be allocated to students is: " << result << endl;
+    }
+
+    return 0;
 }
