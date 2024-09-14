1c7bf5e240654e82c99b296cf98334f95edd5a0f
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2024-09-14 12:07:41 +0530

    Bubble sort

---
+++ Practice problems on DSA/Algorithms_and_questions/Sorting_Algorithms/Bubble_Sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+void Bubble_Sort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int swapped = false;
+        for (int j = 0; j < n - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swapped = true;
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+        if (swapped == false)
+        {
+            break;
+        }
+    }
+    cout << "Array Elements: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+int main()
+{
+    int arri[] = {2, 44, 5, 3, 44, 2, 67, 898};
+    Bubble_Sort(arri, (sizeof(arri) / sizeof(arri[1])));
+    return 0;
+}
