baa15096e0a46d98212ed913e601c37385980754
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2024-09-14 12:10:46 +0530

    .

--- Practice problems on DSA/Algorithms_and_questions/Sorting_Algorithms/Selection_Sort.cpp
+++ Practice problems on DSA/Algorithms_and_questions/Sorting_Algorithms/Selection_Sort.cpp
@@ -1,32 +1,31 @@
 #include <iostream>
+//  https://bit.ly/3pvCTz9
 
 using namespace std;
 
-void selectionSort(int arr[], int n){
-    for (int i = 0; i < n-1; i++)
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
     {
         int minIndex = i;
-        for (int j = i+1 ; j < n; j++)
+        for (int j = i + 1; j < n; j++)
         {
-            if (arr[minIndex]> arr[j])
+            if (arr[minIndex] > arr[j])
             {
                 minIndex = j;
             }
-            
-            
         }
         swap(arr[i], arr[minIndex]);
-        
     }
-    
 }
-int main() {
-    int arr1[] = {2,3,1,4,5,7,6,8};
+int main()
+{
+    int arr1[] = {2, 3, 1, 4, 5, 7, 6, 8};
     selectionSort(arr1, 8);
     for (int i = 0; i < 8; i++)
     {
-        cout<<arr1[i]<<" ";
+        cout << arr1[i] << " ";
     }
-    
+
     return 0;
 }
