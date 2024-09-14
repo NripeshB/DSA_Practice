341b2defa23690c3051b7fdeab2a79ad62a6ec98
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2024-09-14 22:24:20 +0530

    Insertion Sort algorithm

---
+++ Practice problems on DSA/Algorithms_and_questions/Sorting_Algorithms/Insertion_Sort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+void InsertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int j = i - 1;
+        int check = arr[j + 1];
+        while (j >= 0 && arr[j] > check)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = check;
+    }
+}
+
+int main()
+{
+
+    return 0;
+}
