33e2a0ee354f97217bd171e339cc3b603156e505
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2024-09-21 11:58:36 +0530

    Merging two arrays into the same one

--- Practice problems on DSA/Algorithms_and_questions/Array Questions/Merging_Two_arrays.cpp
+++ Practice problems on DSA/Algorithms_and_questions/Array Questions/Merging_Two_arrays.cpp
@@ -9,7 +9,7 @@ public:
     void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
     {
         int i = 0, j = 0, k = 0;
-        vector<int> arr;
+        int arr[m + n];
         while (i < m && j < n)
         {
             if (nums1[i] > nums2[j])
@@ -19,20 +19,20 @@ public:
 
             else
             {
-                arr[k++] = nums2[j++];
+                arr[k++] = nums1[i++];
             }
         }
 
-        while (i > m)
+        while (i < m)
+        {
+            arr[k++] = nums1[i++];
+        }
+
+        while (j < n)
         {
             arr[k++] = nums2[j++];
         }
-
-        while (j > n)
-        {
-            arr[k++] = nums2[i++];
-        }
-        for (int i = 0; i <= arr.size(); i++)
+        for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
         {
             nums1[i] = arr[i];
         }
