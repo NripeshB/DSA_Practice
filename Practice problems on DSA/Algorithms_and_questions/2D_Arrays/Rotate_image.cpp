7fbea3693af9e3ee201fcb554d57f022a28fb5a7
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2024-10-13 14:19:49 +0530

    How to rotate a matrix by 90 degrees

---
+++ Practice problems on DSA/Algorithms_and_questions/2D_Arrays/Rotate_image.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    void rotate(vector<vector<int>> &matrix)
+    {
+        int rows = matrix.size();
+        int col = matrix[0].size();
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = i + 1; j < col; j++)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        int start = 0;
+        int end = col;
+
+        for (int i = 0; i < rows; i++)
+        {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
