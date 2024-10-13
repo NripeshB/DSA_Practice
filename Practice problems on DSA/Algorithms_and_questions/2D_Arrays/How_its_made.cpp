d57bdee7c4816fd077eefeb3196bcf655b1c8f61
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2024-10-13 10:04:10 +0530

    2-D arrays implementation

---
+++ Practice problems on DSA/Algorithms_and_questions/2D_Arrays/How_its_made.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+// first method
+
+int a[3][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+
+// second method
+
+int b[3][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+
+// third method: by input
+
+int main()
+{
+
+    int arr[3][4];
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << arr[i][j];
+            cout << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
