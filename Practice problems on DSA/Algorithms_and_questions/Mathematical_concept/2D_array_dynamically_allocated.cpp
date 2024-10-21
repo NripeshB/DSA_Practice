7ba54c68d3127b4eda778e0cf1ff756895499d60
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2024-10-21 13:12:43 +0530

    It dynamically allocates space and deletes the same for a 2D array

---
+++ Practice problems on DSA/Algorithms_and_questions/Mathematical_concept/2D_array_dynamically_allocated.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+void free_memory(int **arr, int n, int m)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            delete[] arr[j];
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    int m;
+    cout << "enter the first number: " << endl;
+    cin >> n;
+    cout << "enter the second number: " << endl;
+    cin >> m;
+
+    int **arr = new int *[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = new int[m];
+    }
+
+    free_memory(arr, n, m);
+
+    return 0;
+}
