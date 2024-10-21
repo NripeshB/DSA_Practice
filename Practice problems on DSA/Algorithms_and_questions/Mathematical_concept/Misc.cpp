e519b98c175d3fb0fc925c0bbe248021faa0c3bb
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2024-10-21 13:39:55 +0530

    Using  a macros

---
+++ Practice problems on DSA/Algorithms_and_questions/Mathematical_concept/Misc.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#define PI 3.141
+// defining a macros for the program to use it all throughout the program.
+using namespace std;
+
+double area_of_sphere(int r)
+{
+    return ((4 / 3) * PI * r * r * r);
+}
+int main()
+{
+
+    return 0;
+}
