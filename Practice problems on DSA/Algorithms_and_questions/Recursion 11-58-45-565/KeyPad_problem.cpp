3e621f8d4591e9286fe6f18eca242fe06191ce6b
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2024-11-10 23:35:30 +0530

    The Keypad Problem

---
+++ Practice problems on DSA/Algorithms_and_questions/Recursion 11-58-45-565/KeyPad_problem.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+
+    void Letter_resolution(vector<string> &ans, vector<string> keypad, string digits, int i, string object)
+    {
+        if (i >= digits.length())
+        {
+            ans.push_back(object);
+            return;
+        }
+
+        int key = digits[i] - '0';
+        string iterator = keypad[key];
+        for (int j = 0; j < iterator.length(); j++)
+        {
+
+            Letter_resolution(ans, keypad, digits, i + 1, object + iterator[j]);
+        }
+    }
+
+public:
+    vector<string> letterCombinations(string digits)
+    {
+        if (digits == "")
+        {
+            vector<string> a = {};
+            return a;
+        }
+
+        vector<string> keypad;
+        keypad = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string> ans;
+        Letter_resolution(ans, keypad, digits, 0, "");
+        return ans;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
