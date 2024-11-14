8cb884242d6c851a2f5b8925714712e631d90d73
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2024-11-14 21:37:54 +0530

    use of recursion an backtracking

---
+++ Practice problems on DSA/Algorithms_and_questions/Recursion 11-58-45-565/String_permutation.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+void possible_permutations(string s, int i, vector<string> &answer)
+{
+
+    if (i >= s.length())
+    {
+        answer.push_back(s);
+        return;
+    }
+    for (int j = i; j < s.length(); j++)
+    {
+        swap(s[i], s[j]);
+        possible_permutations(s, i + 1, answer);
+        swap(s[i], s[j]);
+    }
+}
+
+int main()
+{
+    string s = "abcef";
+    vector<string> answer;
+    possible_permutations(s, 0, answer);
+    for (int i = 0; i < answer.size(); i++)
+    {
+        cout << answer[i] << endl;
+    }
+
+    return 0;
+}
