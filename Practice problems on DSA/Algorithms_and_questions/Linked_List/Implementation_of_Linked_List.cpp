ff536d7b416ab9ff903fb56c54536c83128928d2
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2024-11-21 15:36:24 +0530

    Insert at had function and traversal of linked list

--- Practice problems on DSA/Algorithms_and_questions/Linked_List/Implementation_of_Linked_List.cpp
+++ Practice problems on DSA/Algorithms_and_questions/Linked_List/Implementation_of_Linked_List.cpp
@@ -6,19 +6,44 @@ class Node
 {
 public:
     int data;
+    // here its node *next as next is a pointer to another node and not to an integer.
     Node *next;
 
+    // creating a constructor for the Node.
     Node(int data)
     {
         this->data = data;
         this->next = NULL;
     }
 };
+void Insert_at_Head(Node *&head, int d)
+{
+    // creating a temp node which saves the address fort he prev head and takes up the head title.
+    Node *temp = new Node(d);
+    temp->next = head;
+    head = temp;
+}
+
+void Print(Node *&head)
+{
+    // pointing the temp node at head and then traversing it.
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
 
 int main()
 {
     Node *Node1 = new Node(10);
     cout << Node1->data << endl;
     cout << Node1->next << endl;
+    // creating a head for the first node.
+    Node *head = Node1;
+    Insert_at_Head(Node1, 12);
+    Print(Node1);
+
     return 0;
 }
