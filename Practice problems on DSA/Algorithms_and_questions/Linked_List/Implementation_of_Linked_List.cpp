633cf648556da2f04ba763b207a7af0201e12340
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2024-11-21 14:03:34 +0530

    Linked list basic implementation

---
+++ Practice problems on DSA/Algorithms_and_questions/Linked_List/Implementation_of_Linked_List.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+int main()
+{
+    Node *Node1 = new Node(10);
+    cout << Node1->data << endl;
+    cout << Node1->next << endl;
+    return 0;
+}
