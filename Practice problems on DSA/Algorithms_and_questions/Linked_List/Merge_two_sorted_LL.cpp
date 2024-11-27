14e7895bf9397dba5d428f4146bfd4e7f0e7323a
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2024-11-27 08:16:10 +0530

    Merge_two_sorted_LL

---
+++ Practice problems on DSA/Algorithms_and_questions/Linked_List/Merge_two_sorted_LL.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+
+    
+    template <typename T>
+    class Node {
+        public:
+        T data;
+        Node* next;
+
+        Node(T data) {
+            next = NULL;
+            this->data = data;
+        }
+
+        ~Node() {
+            if (next != NULL) {
+                delete next;
+            }
+        }
+    };
+
+
+Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
+{
+    if(first ==NULL){
+        return second;
+    }
+    if(second == NULL){
+        return first;
+    }
+    Node<int>* HEAD = NULL;
+    Node<int>* TEMP = NULL;
+
+    Node<int>* i = first;
+    Node<int>* j = second;
+
+    if(i->data >= j->data){
+        HEAD = j;
+        TEMP = j;
+        j = j->next;
+    }
+    else{
+        HEAD = i;
+        TEMP = i;
+        i = i->next;
+    }
+    while(i != NULL && j != NULL){
+        if(i->data >= j->data){
+            TEMP->next = j;
+            TEMP = j;
+            j = j->next;
+        }
+        else if(i->data < j->data){
+            TEMP ->next = i;
+            TEMP = i;
+            i = i->next;
+        }
+    }
+
+    while(i != NULL){
+        TEMP->next = i;
+        TEMP = i;
+        i=i->next;
+
+    }
+    while(j !=NULL){
+        TEMP ->next = j;
+        TEMP = j;
+        j = j->next;
+    }
+
+    return HEAD;
+
+
+}
+
+
+int main() {
+    return 0;
+}
