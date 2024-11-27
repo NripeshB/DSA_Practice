13b060e6587da1eef134c2a29ae8274e5d0f41e6
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2024-11-27 07:41:54 +0530

    Added a second approach

--- Practice problems on DSA/Algorithms_and_questions/Linked_List/Sort0_1_2.cpp
+++ Practice problems on DSA/Algorithms_and_questions/Linked_List/Sort0_1_2.cpp
@@ -23,7 +23,7 @@ public:
     }
 };
 
-
+//Approach One, with changing the data;
 Node* sortList(Node *head){
     Node*temp= head;
 
@@ -63,6 +63,65 @@ Node* sortList(Node *head){
 
     return head;
 }
+//Approach Two, without changing the data:
+
+void insertAtTail(Node*&tail, Node*curr){
+    tail->next = curr;
+    tail= curr;
+}
+
+Node *sortList(Node *head) {
+     Node *head0 = new Node(-1);
+     Node* tail0 = head0;
+     Node *head1 = new Node(-1);
+     Node* tail1 = head1;
+     Node *head2 = new Node(-1);
+     Node* tail2 = head2;
+    
+
+    Node *curr = head;
+
+    while(curr!=NULL){
+        int value = curr->data;
+
+        if(value == 0){
+            insertAtTail(tail0,curr);
+        }
+        else if(value == 1){
+            insertAtTail(tail1,curr);
+        }
+        else if(value == 2){
+            insertAtTail(tail2,curr);
+        }
+        curr = curr->next;
+    }
+
+    //merging the three sublists:
+    
+     if(head1 ->next !=NULL){
+         tail0->next = head1->next;
+         tail1->next = head2->next;
+         tail2->next =NULL;
+     }
+     else{
+         tail0->next = head2->next;
+         tail2->next = NULL;
+     }
+      
+      if(head0->next !=NULL){
+          head = head0->next;
+      }
+      else if(head1->next != NULL){
+          head = head1->next;
+      }
+      return head;
+     
+
+
+
+
+
+    }
 int main() {
     return 0;
 }
