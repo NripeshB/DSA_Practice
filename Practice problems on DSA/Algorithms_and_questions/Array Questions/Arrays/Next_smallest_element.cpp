0852446999c503595fcb2dafb4ada42fc598e07f
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2024-12-12 21:13:55 +0530

    Next smallest element using stack.

--- Practice problems on DSA/Algorithms_and_questions/Array Questions/Arrays/Next_smallest_element.cpp
+++ Practice problems on DSA/Algorithms_and_questions/Array Questions/Arrays/Next_smallest_element.cpp
@@ -1,7 +1,9 @@
+//https://www.naukri.com/code360/problems/next-smaller-element_1112581?topList=love-babbar-dsa-sheet-problems&leftPanelTab=0%3Fsource%3Dyoutube&campaign=Lovebabbarcodestudio&utm_source=youtube&utm_medium=affiliate&utm_campaign=Lovebabbarcodestudio&leftPanelTabValue=SUBMISSION
 #include <iostream>
+#include<stack>
 using namespace std;
 
-void func(vector<int> &arr, vector<int> &ans, int n, int i) {
+/*void func(vector<int> &arr, vector<int> &ans, int n, int i) {
     if (i >= n) {
         return;
     }
@@ -25,8 +27,33 @@ vector<int> nextSmallerElement(vector<int> &arr, int n) {
     vector<int> Ans(n,-1); // Ensure the result vector is properly sized with default values
     func(arr, Ans, n, 0);
     return Ans;
+}*/
+
+
+//Stack Implementation code:
+
+
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    stack<int> s;
+    s.push(-1);
+    vector<int> ans(n);
+    
+    for(int i=n-1; i>=0 ; i--) {
+        int curr = arr[i];
+        while(s.top() >= curr)
+        {
+            s.pop();
+        }
+        //ans is stack ka top
+        ans[i] = s.top();
+        s.push(curr);
+    }
+    return ans;
 }
+
+
 
 int main() {
     return 0;
-}
+}   
