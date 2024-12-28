e83e947ba361a2c2e881c39103a8c9a88f30e96a
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2024-12-28 10:19:21 +0530

    Min_max_sum_in_window

---
+++ Practice problems on DSA/Algorithms_and_questions/Queues/Min_max_sum_in_window.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+
+long long sumOfMaxAndMin(vector<int> &nums, int n, int k) {
+	deque<int> maxindex;
+	deque<int> minindex;
+
+	long long sum = 0;
+	int mini =  0;
+	int maxi = 0;
+	for(int i=0; i<k;i++){
+		while (!maxindex.empty() && nums[maxindex.back()] <= nums[i]) {
+            maxindex.pop_back();
+        }
+        maxindex.push_back(i);
+
+        // Maintain increasing order in minindex
+        while (!minindex.empty() && nums[minindex.back()] >= nums[i]) {
+            minindex.pop_back();
+        }
+        minindex.push_back(i);
+
+	}
+    sum += nums[maxindex.front()] + nums[minindex.front()];
+
+
+	for(int i = k; i<n; i++){
+		
+		while(!maxindex.empty() &&maxindex.front() <= i-k){
+			maxindex.pop_front();
+		}
+		
+		while(!minindex.empty() &&minindex.front() <= i-k){
+			minindex.pop_front();
+		}
+		while(!maxindex.empty() &&nums[maxindex.back()] <= nums[i]){
+			maxindex.pop_back();
+		}
+		maxindex.push_back(i);
+		while(!minindex.empty() && nums[minindex.back()] >= nums[i]){
+			minindex.pop_back();
+		}
+		minindex.push_back(i);
+
+        
+		sum += nums[maxindex.front()] + nums[minindex.front()];
+
+        }
+		return sum;
+		
+}
+
+
+int main() {
+    return 0;
+}
