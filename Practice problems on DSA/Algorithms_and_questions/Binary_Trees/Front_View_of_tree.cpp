c4d4c1e00913dece96ed3e33c67b0b402cd744b4
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2024-12-29 18:37:50 +0530

    .

---
+++ Practice problems on DSA/Algorithms_and_questions/Binary_Trees/Front_View_of_tree.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+
+        
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    vector<int> topView(Node *root)
+    {
+        vector<int> ans;
+        map<int, int> topnode;
+        
+        queue<pair<Node*, int>> q;
+        q.push(make_pair(root, 0));
+        
+        while(!q.empty()){
+            pair<Node*, int> temp = q.front();
+            q.pop();
+            
+            Node* frontNode = temp.first;
+            int hd = temp.second;
+            
+            if(topnode.find(hd) == topnode.end()){
+                topnode[hd] = frontNode->data; 
+            }
+            if(frontNode->left){
+                q.push(make_pair(frontNode->left, hd-1));
+            }
+            if(frontNode->right){
+                q.push(make_pair(frontNode->right, hd+1));
+            }
+        }
+        for(auto i:topnode){
+            ans.push_back(i.second);
+        }
+        return ans;
+    }
+
+};
+
+int main() {
+    return 0;
+}
