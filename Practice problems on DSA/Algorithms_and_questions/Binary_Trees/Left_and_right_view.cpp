99c0225ac54a0d97b33c24d4125a3adff885eef2
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2024-12-29 17:01:00 +0530

    left and right view of a binary tree

---
+++ Practice problems on DSA/Algorithms_and_questions/Binary_Trees/Left_and_right_view.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+//the following is for left but for right view the right fucntion would be called first 
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; 
+
+// Should return  right view of tree
+class Solution
+{
+    public:
+    void Left(Node* root, vector<int>&ans,int level){
+      if(root == NULL){
+          return ;
+      }
+      
+      if(level == ans.size() ){
+          ans.push_back(root->data);
+      }
+      Left(root->left,ans, level+1);
+      Left(root->right, ans, level+1);
+    
+      
+  }
+    //Function to return list containing elements of right view of binary tree.
+    vector<int> leftView(Node *root)
+    {
+       vector<int>ans;
+        Left(root, ans, 0);
+        return ans;
+    }
+};
+
+
+int main() {
+    return 0;
+}
