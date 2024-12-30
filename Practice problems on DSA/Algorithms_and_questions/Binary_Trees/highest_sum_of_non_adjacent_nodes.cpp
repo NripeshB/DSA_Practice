095a193ace29253ec3650e0695538f7c4fcd1d11
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2024-12-30 07:01:29 +0530

    highest_sum_of_non_adjacent_nodes

---
+++ Practice problems on DSA/Algorithms_and_questions/Binary_Trees/highest_sum_of_non_adjacent_nodes.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+class Solution{
+  public:
+    pair<int, int> solve(Node*root){
+        if(root == NULL){
+            pair<int, int> p = make_pair(0, 0);
+            return p;
+        }
+        
+        pair<int,int> left = solve(root->left);
+        pair<int,int> right = solve(root->right);
+        
+        pair<int, int> res;
+        res.first = root->data+left.second + right.second;
+        res.second = max(left.first, left.second) + max(right.first, right.second);
+        return res;
+    }
+
+    int getMaxSum(Node *root) 
+    {
+        pair<int, int> p = solve(root);
+        int ans = max(p.first, p.second);
+        return ans;
+    }
+};
+
+int main() {
+    return 0;
+}
