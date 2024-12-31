7ec00534c6a5e7c5635a3c7546973af58216ef7b
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2024-12-31 15:36:25 +0530

    .

--- Practice problems on DSA/Algorithms_and_questions/Sorting_Algorithms/Quick_sort.cpp
+++ Practice problems on DSA/Algorithms_and_questions/Sorting_Algorithms/Quick_sort.cpp
@@ -2,63 +2,42 @@
 
 using namespace std;
 
-int Partition(int arr[], int s, int e)
-{
-    int pivot = arr[s];
-    int count = 0;
+int partition(vector<int>& arr, int low, int high) {
+  
+    // Choose the pivot
+    int pivot = arr[high];
+  
+    // Index of smaller element and indicates 
+    // the right position of pivot found so far
+    int i = low - 1;
 
-    for (int i = s + 1; i <= e; i++)
-    {
-        if (arr[i] <= pivot)
-        {
-            count++;
-        }
-    }
-    int pivotindex = s + count;
-    swap(arr[s], arr[pivotindex]);
-
-    int i = s;
-    int j = e;
-
-    while (i < pivotindex && j > pivotindex)
-    {
-        while (arr[i] < pivot)
-        {
+    // Traverse arr[;ow..high] and move all smaller
+    // elements on left side. Elements from low to 
+    // i are smaller after every iteration
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
             i++;
-        }
-        while (arr[j] > pivot)
-        {
-            j--;
-        }
-
-        if (i < pivotindex && j > pivotindex)
-        {
-            swap(arr[i++], arr[j--]);
+            swap(arr[i], arr[j]);
         }
     }
-    return pivotindex;
+    
+    // Move pivot after smaller elements and
+    // return its position
+    swap(arr[i + 1], arr[high]);  
+    return i + 1;
 }
 
-void Quicksort(int arr[], int s, int e)
-{
-    if (s >= e)
-    {
-        return;
+// The QuickSort function implementation
+void quickSort(vector<int>& arr, int low, int high) {
+  
+    if (low < high) {
+      
+        // pi is the partition return index of pivot
+        int pi = partition(arr, low, high);
+
+        // Recursion calls for smaller elements
+        // and greater or equals elements
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
     }
-
-    int part = Partition(arr, s, e);
-    Quicksort(arr, s, part - 1);
-    Quicksort(arr, part + 1, e);
-}
-
-int main()
-{
-    int arr[] = {2, 3, 5, 2, 6, 8, 7, 10, 9, 4};
-    Quicksort(arr, 0, 9);
-    for (int i = 0; i <= 9; i++)
-    {
-        cout << arr[i] << endl;
-    }
-
-    return 0;
 }
