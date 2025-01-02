4a5613847b1505155d24218e3d4949ddc6539936
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2025-01-02 13:00:08 +0530

    .

--- Practice problems on DSA/Algorithms_and_questions/Binary_search_Trees/Implementation_of_BST.cpp
+++ Practice problems on DSA/Algorithms_and_questions/Binary_search_Trees/Implementation_of_BST.cpp
@@ -84,6 +84,70 @@ void levelOrderTraversal(Node* root){
     }
 }
 
+int minval(Node* root){
+    while (root->left !=NULL)
+    {
+        root = root->left;
+    }
+    return root->data; 
+}
+
+Node* DeleteNode(Node* root, int x){
+    if (root == NULL)
+    {
+        return root;
+    }
+
+    if (root->data == x)
+    {
+        // 0 children
+        if (root->left == NULL && root->right == NULL)
+        {
+            delete root;
+            return NULL;
+        }
+        
+
+        //1 child
+        if (root->left != NULL && root->right == NULL)
+        {
+            Node*temp  = root->left;
+            delete root;
+            return temp;
+        }
+        
+        if (root->right != NULL && root->left == NULL)
+        {
+            Node*temp  = root->right;
+            delete root;
+            return temp;
+        }
+        
+
+        // 2 children
+        //either take the minimum value from the right subtree or the maximum value from the left subtree
+         if (root->left != NULL && root->right != NULL)
+        {
+            int mini = minval(root->right);
+            root->data = mini;
+            root->right = DeleteNode(root->right, mini);
+            return root;
+            
+        }
+    }
+    
+
+    else if(root->data > x){
+        root->left = DeleteNode(root->left, x);
+        return root;
+    }
+    else if(root->data < x){
+        root->right = DeleteNode(root->right, x);
+        return root;
+    }
+    
+}
+
 
 //Inorder traversal. Follows the LNR traversal
 //Inorder of BST is in increasing order
@@ -141,6 +205,21 @@ int main() {
     Node* root = NULL;
     cout<<"Enter the data for the root Node"<<endl;
     TakeInput(root);
+
+    cout<<endl<<"level order traversal output: "<<endl;
+    levelOrderTraversal(root);
+    cout<<endl<<"Inorder output: "<<endl;
+
+    Inorder(root);
+    cout<<endl<<"preorder output: "<<endl;
+
+    Preorder(root);
+    cout<<endl<<"postorder output: "<<endl;
+
+    Postorder(root);
+    root = DeleteNode(root, 7);
+
+    //After Deletion
 
     cout<<endl<<"level order traversal output: "<<endl;
     levelOrderTraversal(root);
