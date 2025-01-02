3479624835a4df712d3645d9f001fb5f0e7cd1b3
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2025-01-02 20:24:12 +0530

    LCA of a BST

---
+++ Practice problems on DSA/Algorithms_and_questions/Binary_search_Trees/Finding_LCA_forBST.cpp
@@ -0,0 +1,37 @@
+//https://www.naukri.com/code360/problems/lca-in-a-bst_981280?leftPanelTab=0&leftPanelTabValue=SUBMISSION
+#include <iostream>
+using namespace std;
+
+
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+
+
+
+
+    TreeNode *LCAinaBST(TreeNode *root, TreeNode *P, TreeNode *Q)
+{
+    if(root == NULL){
+        return NULL;
+    }
+
+    if(root->data< P->data && root->data < Q->data){
+        return LCAinaBST(root->right, P, Q);
+    }
+    if(root->data > P->data && root->data > Q->data){
+        return LCAinaBST(root->left, P, Q);
+    }
+    return root;
+}
+    
+
+int main() {
+    return 0;
+}
