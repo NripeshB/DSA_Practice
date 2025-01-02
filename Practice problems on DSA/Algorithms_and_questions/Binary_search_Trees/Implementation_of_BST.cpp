a78efd29aad7833be8f67397caa3a1f71e21129e
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2025-01-02 08:24:23 +0530

    Implementation of binary search tree

---
+++ Practice problems on DSA/Algorithms_and_questions/Binary_search_Trees/Implementation_of_BST.cpp
@@ -0,0 +1,157 @@
+#include <iostream>
+using namespace std;
+
+
+class Node{
+    public:
+
+    int data;
+    Node* left;
+    Node* right;
+    Node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+
+
+};
+
+Node* BST(Node* root, int d){
+    if (root == NULL)
+    {
+        root = new Node(d);
+        return root;
+
+    }
+
+    if (d <= root->data)
+    {
+        root->left = BST(root->left, d);
+    }
+    else if(d > root->data){
+        root->right = BST(root->right, d);
+    }
+    
+    return root;
+    
+    
+}
+
+
+void TakeInput(Node*& root){
+    int d;
+    cin>>d;
+    while( d!= -1){
+        root = BST(root, d);
+        cin>>d;
+    }
+}
+
+//Implementation of Level Order Traversal.
+void levelOrderTraversal(Node* root){
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()){
+        Node*temp = q.front();
+        q.pop();
+
+
+        if (temp == NULL)
+        {
+            cout<<endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+            
+                }
+
+        else{ 
+            cout<<temp->data<< " ";
+            
+            if(temp->left){
+            q.push(temp->left);
+        }
+
+        if(temp->right){
+            q.push(temp->right);
+            }
+        }      
+    }
+}
+
+
+//Inorder traversal. Follows the LNR traversal
+void Inorder(Node*root){
+    if (root == NULL)
+    {
+        return ;
+    }
+//left
+    Inorder(root->left);
+//printing node
+    cout<<root->data<< " ";
+//right
+    Inorder(root->right);
+
+    }
+
+
+//Preorder traversal. Follows the NLR traversal
+void Preorder(Node*root){
+    if (root == NULL)
+    {
+        return ;
+    }
+//printing node
+    cout<<root->data<< " ";
+//left
+    Preorder(root->left);
+//right
+    Preorder(root->right);
+
+    }
+    
+
+
+
+//Postorder traversal. Follows the LRN traversal
+void Postorder(Node*root){
+    if (root == NULL)
+    {
+        return ;
+    }
+
+//left
+    Postorder(root->left);
+//right
+    Postorder(root->right);
+//printing node
+    cout<<root->data<< " ";
+
+    }
+    
+
+int main() {
+    Node* root = NULL;
+    cout<<"Enter the data for the root Node"<<endl;
+    TakeInput(root);
+
+    cout<<endl<<"level order traversal output: "<<endl;
+    levelOrderTraversal(root);
+    cout<<endl<<"Inorder output: "<<endl;
+
+    Inorder(root);
+    cout<<endl<<"preorder output: "<<endl;
+
+    Preorder(root);
+    cout<<endl<<"postorder output: "<<endl;
+
+    Postorder(root);
+
+
+    return 0;
+}
