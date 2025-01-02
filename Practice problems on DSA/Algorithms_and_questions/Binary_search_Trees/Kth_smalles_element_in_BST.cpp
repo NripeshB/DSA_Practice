f68f107a4c1999e062bda247f99980970cd847de
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2025-01-02 19:14:58 +0530

    kth smallest element in a BST

---
+++ Practice problems on DSA/Algorithms_and_questions/Binary_search_Trees/Kth_smalles_element_in_BST.cpp
@@ -0,0 +1,49 @@
+//https://www.naukri.com/code360/problems/find-k-th-smallest-element-in-bst_1069333?leftPanelTab=0&leftPanelTabValue=SUBMISSION
+#include <iostream>
+using namespace std;
+
+// you can also save the answer from inorder traversal in a vector and iterate to the kth element
+    class BinaryTreeNode 
+    {
+    public : 
+        int data;
+        BinaryTreeNode  *left;
+        BinaryTreeNode  *right;
+
+        BinaryTreeNode (int data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+
+int solve(BinaryTreeNode* root, int k, int& i){
+    if( root== NULL){
+        return -1;
+    }
+
+    int L = solve(root->left, k, i);
+    if(L != -1){
+        return L;
+    }
+
+    i++;
+    if(i == k){
+        return root->data;
+    }
+    
+    return  solve( root->right, k, i);
+
+    
+}
+int kthSmallest(BinaryTreeNode* root, int k) {
+    // Write your code here.
+    int i =0;
+    int ans = solve(root, k, i);
+    return ans;
+}
+
+int main() {
+    return 0;
+}
