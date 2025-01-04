2800e5301233d6e6a5ab6d9cab72b34ffd3bcb80
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2025-01-04 07:38:08 +0530

    Minimum cost of rope

---
+++ Practice problems on DSA/Algorithms_and_questions/Heaps/Minimum_cost_of_rope.cpp
@@ -0,0 +1,36 @@
+//https://geeksforgeeks.org/problems/minimum-cost-of-ropes-1587115620/1
+#include <iostream>
+using namespace std;
+
+class Solution {
+  public:
+    
+    int minCost(vector<int>& arr) {
+        priority_queue<int, vector<int>, greater<int> > p;
+        for(int i = 0; i< arr.size(); i++){
+            p.push(arr[i]);
+        }
+        
+        int ans = 0;
+        while(p.size() > 1){
+            int sum = 0;
+            
+            int a = p.top();
+            p.pop();
+            int b = p.top();
+            p.pop();
+            
+            sum = a+b;
+            p.push(sum);
+            ans+= sum;
+            
+        }
+        
+        return ans;
+        
+    }
+};
+
+int main() {
+    return 0;
+}
