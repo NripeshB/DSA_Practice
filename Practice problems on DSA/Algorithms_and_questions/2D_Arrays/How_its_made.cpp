4ff73ed6751f1fac85249089208086496e689b6e
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2025-01-06 16:42:41 +0530

    .

--- Practice problems on DSA/Algorithms_and_questions/2D_Arrays/How_its_made.cpp
+++ Practice problems on DSA/Algorithms_and_questions/2D_Arrays/How_its_made.cpp
@@ -1,21 +1,14 @@
 #include <iostream>
 using namespace std;
-
 // first method
-
 int a[3][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-
 // second method
-
 int b[3][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
-
 // third method: by input
 
 int main()
 {
-
     int arr[3][4];
-
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 4; j++)
@@ -32,6 +25,5 @@ int main()
         }
         cout << endl;
     }
-
     return 0;
 }
