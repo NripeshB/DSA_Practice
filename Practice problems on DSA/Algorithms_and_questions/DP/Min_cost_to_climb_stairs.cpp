75488699dd1d932b92fe627e32d041f045c949b7
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2025-01-16 14:39:06 +0530

    Min_cost_to_climb_stairs

--- Practice problems on DSA/Algorithms_and_questions/DP/Min_cost_to_climb_stairs.cpp
+++ Practice problems on DSA/Algorithms_and_questions/DP/Min_cost_to_climb_stairs.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 class Solution {
 public:
-
+//TC O(n); SC O(n) + O(n)
     int solve(int size, vector<int>&cost, vector<int>&dp){
         //returning the memoization if exists
         if(dp[size] != -1){
@@ -33,5 +33,51 @@ public:
 
         int ans =  solve(size, cost, dp);
         return ans;
+    }
+};
+
+//TC O(n); SC O(n) 
+//bottom up method
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int size = cost.size();
+        //using this to implement tabulation approach
+        int dp[size];
+        dp[0] = cost[0];
+        dp[1] = cost[1]; 
+
+        for (int i = 2; i < size; i++)
+        {
+            dp[i]  = cost[i] + min(dp[i-1], dp[i-2]);
+        }
+
+        return min(dp[size-1], dp[size -2]);
+
+    }
+};
+
+//TC O(n); SC O(1) 
+//optimised space
+//bottom up method
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int size = cost.size();
+        //using this to implement tabulation approach
+        
+        int prev1 = cost[0];
+        int prev2 = cost[1]; 
+        int curr;
+
+        for (int i = 2; i < size; i++)
+        {
+            curr  = cost[i] + min(prev1, prev2);
+            prev1 = prev2;
+            prev2 = curr;
+        }
+
+        return min(prev1, prev2);
+
     }
 };
