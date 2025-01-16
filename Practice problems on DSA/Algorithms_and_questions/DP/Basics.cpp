78ca9c29cb05e811c0363297aca1084e5bc3e3e0
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2025-01-16 14:04:30 +0530

    basics of DP

--- Practice problems on DSA/Algorithms_and_questions/DP/Basics.cpp
+++ Practice problems on DSA/Algorithms_and_questions/DP/Basics.cpp
@@ -49,6 +49,30 @@ public:
 };
 
 
+//Implementing the tabulation method and OPTIMISING IT
+class Solution {
+public:
+    int fib(int n) {
+
+        if(n == 1 || n == 0){
+            return n;
+        }
+        int prev1 = 0; 
+        int prev2 = 1; 
+        int curr;
+
+        for(int i = 2; i <= n; i++){
+           curr = prev1+ prev2;
+           prev1 = prev2;
+           prev2 = curr;
+        }
+
+        return curr;
+
+    }
+};
+
+
 int main() {
     return 0;
 }
