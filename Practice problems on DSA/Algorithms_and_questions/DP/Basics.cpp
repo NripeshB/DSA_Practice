2afb0394c2ccad45dfaa3afd23e0582b2f522db2
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2025-01-16 12:11:02 +0530

    added a lil things

--- Practice problems on DSA/Algorithms_and_questions/DP/Basics.cpp
+++ Practice problems on DSA/Algorithms_and_questions/DP/Basics.cpp
@@ -26,6 +26,28 @@ int fib(int n) {
     return ans[n]; // Return the stored result.
 }
 
+//Implementing the tabulation method on a simple question
+class Solution {
+public:
+    int fib(int n) {
+
+        if(n == 1 || n == 0){
+            return n;
+        }
+        int arr[n+1];
+        arr[0] = 0;
+        arr[1] = 1;
+
+
+        for(int i = 2; i <= n; i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+
+        return arr[n];
+
+    }
+};
+
 
 int main() {
     return 0;
