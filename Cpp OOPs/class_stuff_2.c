471e6b3d73311535b4fff6091c27f9bd3c45cfa9
Nripesh Bhusal <nripesh.bhusal@gmail.com>
2025-02-11 09:29:55 +0530

    class stuff

---
+++ Cpp OOPs/class_stuff_2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+    
+    int a;
+    int b;
+    int c;
+    printf("Enter the height of the rec: ");
+    printf("Enter the height of the rec: ");
+    scanf("%d %d %d", &a, &b ,&c);
+    if(a>b && a>b){
+        printf("%d is the greatest number.", a);
+    }
+    else if(b>a && b>c){
+        printf("%d is the greatest number.", b);
+    }
+    if(c>b && c>a){
+        printf("%d is the greatest number.", c);
+    }
+    
+
+    // int num;
+
+    // printf("Enter a number: ");
+    // scanf("%d", &num);
+
+    // //for singular sentences we can choose to not put braces.
+    // if(num%2== 0)
+    //     printf("%d is an even number", num );
+    
+    // else
+    //     printf("%d is an odd number", num );
+    
+}
